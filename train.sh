5c24d44e8628d12deea40ab91fdd370498b32545
y-kallel <55721289+y-kallel@users.noreply.github.com>
2020-07-14 20:09:53 -0700

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 export PYTHONPATH=":/content/models/research/:/content/models/research/slim/"
 
-python3 /content/tensorflow_retinanet/tensorflow_retinanet/object_detection/model_main.py \
+python3 model_main.py \
 --model_dir="train" --pipeline_config_path="ssd_resnet50_v1_ppn.config" 
